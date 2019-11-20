ed2bbd6bb8249cd3cbfe8271bcc414cf07feb037
Marcos Volpato <volpatomv@gmail.com>
2019-11-20 11:40:24 -0300

    melhorado os metodos do dao

--- src/main/java/models/dao/BookDao.java
+++ src/main/java/models/dao/BookDao.java
@@ -8,65 +8,76 @@ import models.Book;
 
 public class BookDao {
 	protected SessionFactory sessionFactory;
-	
+
 	public BookDao() {
 		sessionFactory = SessionManager.getInstance().getSessionFactory();
 	}
-	public void create() {
-    	
-    	Book book = new Book();
-        book.setTitle("Java amateurs vol2");
-        book.setAuthor("Marcos Vinicius Volpato");
-        book.setPrice(32.59f);
-        
-    	Session session = sessionFactory.openSession();
-    	session.beginTransaction();
-    	
-    	session.save(book);
-    	
-    	session.getTransaction().commit();
-    	session.close();
-    }
- 
-    public void read() {
-    	Session session = sessionFactory.openSession();
-    	 
-        long bookId = 1;
-        Book book = session.get(Book.class, bookId);
-     
-        System.out.println("Title: " + book.getTitle());
-        System.out.println("Author: " + book.getAuthor());
-        System.out.println("Price: " + book.getPrice());
-     
-        session.close();
-    }
- 
-    public void update() {
-    	Book book = new Book();
-        book.setId(1);
-        book.setTitle("Ultimate Java Programming");
-        book.setAuthor("Nam Ha Minh");
-        book.setPrice(19.99f);
-     
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-     
-        session.update(book);
-     
-        session.getTransaction().commit();
-        session.close();
-    }
- 
-    public void delete() {
-    	Book book = new Book();
-        book.setId(1);
-     
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-     
-        session.delete(book);
-     
-        session.getTransaction().commit();
-        session.close();
-    }
+	
+	public Book create(Book book) {
+		boolean success = true;
+		Session session = sessionFactory.openSession();
+		try {
+			session.beginTransaction();
+
+			session.save(book);
+
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			success = false;
+		}
+		session.close();
+		
+		return success ? book : null;
+	}
+
+	public Book read(long id) {
+		Session session = sessionFactory.openSession();
+
+		Book book = session.get(Book.class, id);
+
+		session.close();
+		return book;
+	}
+
+	public Book update(Book book) {
+		
+		boolean success = true;
+
+		Session session = sessionFactory.openSession();
+		try {
+			session.beginTransaction();
+
+			session.update(book);
+
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			success = false;
+		}
+		session.close();
+		
+		return success ? book : null;
+	}
+
+	public boolean delete(long id) {
+		Book book = new Book();
+		book.setId(id);
+
+		Session session = sessionFactory.openSession();
+		boolean success = true;
+		
+		try {
+			session.beginTransaction();
+
+			session.delete(book);
+
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			success = false;
+		}
+		session.close();
+		return success;
+	}
 }
